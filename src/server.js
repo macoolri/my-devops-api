0ad76557c95687c0d8e7be7f241d02aef19e8e38
Мария Назарова <mnazarova@ntr.ai>
2025-07-04 00:55:01 +0700

    Fix MongoDB connection with useUnifiedTopology option

--- src/server.js
+++ src/server.js
@@ -52,7 +52,10 @@ mongoose.set('debug', config.MONGODB_DEBUG);
 mongoose
     .connect(
         config.MONGODB_URI,
-        { useNewUrlParser: true }
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        }
     )
     .then(() => {
         if (process.env.NODE_ENV !== 'test') {
